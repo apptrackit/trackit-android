446514a63a1d5aefe292cff0d11b5d24850705b8
bllbtnd <ballabotond@protonmail.com>
2025-04-13 23:26:45 +0200

    redo

--- app/src/main/java/com/example/lifetracker/ui/components/HistoryItem.kt
+++ app/src/main/java/com/example/lifetracker/ui/components/HistoryItem.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.lifetracker.utils.formatDate
-
+//ds
 @Composable
 fun HistoryItem(
     date: Long,
